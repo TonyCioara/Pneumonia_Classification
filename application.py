00e261ddfb833ece5a1d729fb7426578c6e619ae
Tony Cioara <tonyangelo9707@gmail.com>
2019-05-05 14:32:30 -0700

    Application is working and ready for deployment

--- application.py
+++ application.py
@@ -11,13 +11,13 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import datetime
 
-cred = credentials.Certificate("./ServiceAccountKey.json")
-firebase_app = firebase_admin.initialize_app(cred)
-db = firestore.client()
+# cred = credentials.Certificate("./ServiceAccountKey.json")
+# firebase_app = firebase_admin.initialize_app(cred)
+# db = firestore.client()
 
 application = app = Flask(__name__)
-api = Api(app, version='1.0', title='MNIST Classification', description='CNN for Mnist')
-ns = api.namespace('Make_School', description='Methods')
+api = Api(app, version='1.0', title='Pneumonia Classification', description='Tony Cioara')
+ns = api.namespace('Pneumonia Classification', description='Methods')
 
 single_parser = api.parser()
 single_parser.add_argument('file', location='files',
@@ -35,33 +35,29 @@ class CNNPrediction(Resource):
     image_file = args.file
     image_file.save('last_image.png')
     img = Image.open('last_image.png')
-    image_red = img.resize((28, 28)).convert('1')
+    print("size: ", img.size)
+    image_red = img.resize((150, 150)).convert('RGB')
     image = img_to_array(image_red)
-    print("Image shape:", image.shape)
-    x = image.reshape(1, 28, 28, 1)
+    print("1. Image shape:", image.shape)
+    x = image.reshape(1, 150, 150, 3)
     x = x/255
     with graph.as_default():
       out = model.predict(x)
-    print(out[0])
-    print(np.argmax(out[0]))
-    r = np.argmax(out[0])
+    print("Out:", out[0][0])
+    pred = str(round(out[0][0] * 100))
+    return {'Likelyhood of Pneumonia:': pred + " percent"}
 
-    pred = str(r)
-    savePredictionToDB(pred)
-
-    return {'prediction': pred}
-
-def savePredictionToDB(pred):
-  time = datetime.datetime.now()
-  title = "pred " + time.strftime("%I:%M%p on %B %d, %Y")
-  doc_ref = db.collection(u'userlogs').document(title)
-  doc_ref.set({
-    u'prediction': pred,
-    u'time': time,
-  })
+# def savePredictionToDB(pred):
+#   time = datetime.datetime.now()
+#   title = "pred " + time.strftime("%I:%M%p on %B %d, %Y")
+#   doc_ref = db.collection(u'userlogs').document(title)
+#   doc_ref.set({
+#     u'prediction': pred,
+#     u'time': time,
+#   })
 
 if __name__ == '__main__':
-  # app.run(host='0.0.0.0', port=8000)
-  app.run(debug=True)
+  app.run(host='0.0.0.0', port=8000)
+  # app.run(debug=True)
 
 
